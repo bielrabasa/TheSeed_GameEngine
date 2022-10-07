713de3f49ccc26ed134c9f09b6b548c36a48d5df
Biel Rabasa <bielrabasa@gmail.com>
2022-10-07 09:35:23 +0200

    window changes

--- GameEngine/Globals.h
+++ GameEngine/Globals.h
@@ -28,12 +28,12 @@ enum update_status
 };
 
 // Configuration -----------
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 1024
+#define SCREEN_WIDTH 1080
+#define SCREEN_HEIGHT 720
 #define SCREEN_SIZE 1
 #define WIN_FULLSCREEN false
 #define WIN_RESIZABLE true
 #define WIN_BORDERLESS false
 #define WIN_FULLSCREEN_DESKTOP false
 #define VSYNC true
-#define TITLE "3D Physics Playground"
+#define TITLE "The Seed game engine"
