6933bd41a4c0b8b23fa561c3ea591ae2e97e2e29
Roger <79161186+Draquian@users.noreply.github.com>
2022-10-28 23:02:24 +0200

    hierarchy

--- GameEngine/Hierarchy.cpp
+++ GameEngine/Hierarchy.cpp
@@ -57,6 +57,8 @@ update_status HierarchyWindows::PostUpdate(float dt)
 
 void HierarchyWindows::PrintHierarchy(GameObject* GO, int index)
 {
+	int aux = 0;
+	aux = index;
 	if(GO->parent == nullptr)
 	ImGui::Text(GO->name.c_str());
 
@@ -64,14 +66,17 @@ void HierarchyWindows::PrintHierarchy(GameObject* GO, int index)
 	{
 		for (int i = 0; i <= GO->childs.size() - 1; i++)
 		{
-			for (size_t j = 0; j < index; j++)
+			for (size_t j = 0; j < aux; j++)
 			{
 				ImGui::Text("\t"); ImGui::SameLine();
 			}
 			ImGui::Text("%d", i); ImGui::SameLine();
 			ImGui::Text(GO->childs[i]->name.c_str());
-			if(!GO->childs[i]->childs.empty())
-			PrintHierarchy(GO->childs[i], index++);
+			if (!GO->childs[i]->childs.empty())
+			{
+				aux = aux + 1;
+				PrintHierarchy(GO->childs[i], aux);
+			}
 		}
 	}
 }
